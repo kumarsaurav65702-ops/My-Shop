de1c90daef539d47154cfa9387b2df6c6e89e1be
Saurav <kumarsaurav65701@gmail.com>
2026-01-28 14:03:07 +0530

    product card

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../components/ProductCard'
 
 const Product = () => {
+const [data, setData] = useState([])
+
+//  useEffect(() => {
+//     fetch("https://api.escuelajs.co/api/v1/products")
+//       .then((res) => res.json())
+//       .then((result) => setData(result))
+//       .catch((err) => console.log(err))
+//       console.log(data)
+//   }, [])
+//     
+
+const getData = async () => {
+  const response = await axios.get("https://api.escuelajs.co/api/v1/products")
+  setData(response.data)
+  console.log(response.data)
+}
+
+useEffect(() => {
+ getData()
+}, [])
+
+
   return (
-      <div className="min-h-screen bg-(--c6) flex items-center justify-center">
-  <h1 className="text-3xl text-white text-center ">
-    This is Product Page
-  </h1>
-</div>
+      <div className="min-h-screen bg-(--c5) ">
+     {/* {data.map(function(elem, idx){
+      return <h1>hello {idx}</h1>
+    })} */}
+    <ProductCard/>
+  </div>
   )
 }
 
