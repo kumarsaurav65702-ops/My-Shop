55ae65cb7b8a069e0a393e06c682232f902b56c0
Saurav <kumarsaurav65701@gmail.com>
2026-02-09 23:08:56 +0530

    Added brand name and slogan in footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,21 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-<div className='bg-blue-800 h-64 text-white px-4 py-8'>
-    <div className='bg-red-800 h-14'>This is Footer Section</div>
-</div>
+      <div className='bg-red-950 h-64 text-white px-4 py-8 mb-18 md:mb-0'>
+        <div className=' text-center border-b-2'>
+          <h1 className='font-bold text-3xl'>MyShop</h1>
+          <h3>Shop Smart. Live Better</h3>
+        </div>
+        <div className='bg-blue-800 p-6'>
+          <div>
+            <h4 className='font-semibold underline duration-300 ease-out hover:text-red-500'>Home</h4>
+            <h4 className='font-semibold underline duration-300 ease-in-out hover:text-red-500'>About</h4>
+            <h4 className='font-semibold underline duration-300 ease-in-out hover:text-red-500'>Vendor</h4>
+            <h4 className='font-semibold underline duration-300 ease-in-out hover:text-red-500'>Contact</h4>
+          </div>
+        </div>
+
+      </div>
 
     </>
   )
